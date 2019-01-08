91837aec13a4ed03bd4606faccc7cf12be724ce4
adrianomoda <adrianomodafeitosa@gmail.com>
2019-01-08 14:29:07 -0300

    Navbar, dropdown e table responsivos

--- report-data-ui/src/app/navbar/navbar.component.ts
+++ report-data-ui/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  items: MenuItem[]
 
   constructor() { }
 
   ngOnInit() {
+    this.items = [{
+      label: 'Relatório Cometa Supermercados'
+    }]
   }
 
 }
